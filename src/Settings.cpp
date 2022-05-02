8cdc9ee57a57f3297fce59331694807dc67c658a
nadarbreicq <adrien.bracq@gmail.com>
2022-05-02 12:50:04 +0200

    Add delay to init the robot type

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -6,6 +6,7 @@ namespace Settings{
     bool ROBOT;
 
     void init(){ 
+        delay(500);
         pinMode(Pin::robotSelect, INPUT_PULLUP);
         ROBOT = digitalRead(Pin::robotSelect) ? PRIMARY : SECONDARY ;
     }
