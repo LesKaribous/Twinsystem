598cb32956acb328cd84dc0ba90442ed618913e9
nadarbreicq <adrien.bracq@gmail.com>
2022-05-07 01:49:12 +0200

    change delay value

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -6,10 +6,8 @@ namespace Settings{
     bool ROBOT;
 
     void init(){ 
-        delay(500);
         pinMode(Pin::robotSelect, INPUT_PULLUP);
         ROBOT = digitalRead(Pin::robotSelect) ? PRIMARY : SECONDARY ;
     }
-
 }
 
