54e8e2ac420e79e3b0a04dfe3f4897a0a31141f9
Barbatronic <adrien.bracq@gmail.com>
2023-02-24 12:44:00 +0100

    new strategy

--- src/main.cpp
+++ src/main.cpp
@@ -87,27 +87,68 @@ public :
 		Vec2 cakeMarron1(1125,1275);
 
 		Vec2 b1(225,1775);
+
+		Vec2 b1safe(450,1550);
+		Vec2 b5safe(900,400);
+
+		Vec2 depose1(200,1800);
+		Vec2 depose2(350,1800);
+		Vec2 depose3(450,1800);
+
+		Vec2 retrait1(200,1700);
+		Vec2 retrait2(350,1700);
+		Vec2 retrait3(450,1700);
 		
 		actuators->ungrab(RobotCompass::AB);
 		motion->Go(cakeRose1);
 		actuators->grab(RobotCompass::AB);
 		motion->Align(RobotCompass::BC, GetCompassOrientation(TableCompass::NORTH));
-		delay(1000);
+
 		actuators->ungrab(RobotCompass::BC);
 		motion->Go(cakeJaune1);
 		actuators->grab(RobotCompass::BC);
 		motion->Align(RobotCompass::CA, 45);
-		delay(1000);
+
 		actuators->ungrab(RobotCompass::CA);
 		motion->Go(cakeMarron1);
 		actuators->grab(RobotCompass::CA);
-		delay(1000);
-		motion->Go(b1);
 
 
-		actuators->unlock(RobotCompass::AB);
+		
+		//Dépose du premier Gateau
+		motion->Go(b1safe);
+		motion->Align(RobotCompass::BC, -120);
+		motion->Go(depose1);
+		actuators->ungrab(RobotCompass::BC);
+		delay(500);
 		actuators->unlock(RobotCompass::BC);
+		delay(500);
+		motion->Go(retrait1);
+		actuators->close(RobotCompass::BC);
+		//Dépose du second Gateau
+		motion->Go(b1safe);
+		motion->Align(RobotCompass::AB, -120);
+		motion->Go(depose2);
+		actuators->ungrab(RobotCompass::AB);
+		delay(500);
+		actuators->unlock(RobotCompass::AB);
+		delay(500);
+		motion->Go(retrait2);
+		actuators->close(RobotCompass::AB);
+		//Dépose du troisieme Gateau
+		motion->Go(b1safe);
+		motion->Align(RobotCompass::CA, -120);
+		motion->Go(depose3);
+		actuators->ungrab(RobotCompass::CA);
+		delay(500);
 		actuators->unlock(RobotCompass::CA);
+		delay(500);
+		motion->Go(retrait3);
+		actuators->close(RobotCompass::CA);
+
+		motion->Go(b1safe);
+		motion->Go(b5safe);
+
 		delay(10000);
 
 
