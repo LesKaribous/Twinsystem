d66502dc2574100cd2b0f946725826fad268976e
Jules Topart <jules.topart@gmail.com>
2024-04-11 10:41:32 +0200

    basic avoidance

--- src/services/safety/safety.cpp
+++ src/services/safety/safety.cpp
@@ -1,12 +1,35 @@
 #include "safety.h"
-#include "services/intercom/intercom.h"
+
 #include "os/console.h"
 #include "services/motion/motion.h"
+#include "services/intercom/intercom.h"
+#include "utils/timer/timer.h"
 
 INSTANTIATE_SERVICE(Safety, safety)
+
+void getDistanceCallback(Request& req){
+    int d = req.getResponse().toInt();
+    if(d > 10 && d < 1000) safety.setSafeDistance(d);
+}
 
 void Safety::onAttach(){
 }
 
 void Safety::onUpdate(){
+    RUN_EVERY(
+        if(motion.isMoving()){
+            int streer = RAD_TO_DEG * motion.getAbsoluteTargetDirection(); //We are moving in this direction
+            intercom.sendRequest("getDistance(" + String(streer) + ")", 100, getDistanceCallback);
+        }
+    , 100)
+
+    if(motion.isMoving() && m_currentDistance < 300){
+        motion.pause();
+    }else if(motion.isPaused() && m_currentDistance > 300){
+        motion.resume();
+    }
+}
+
+void Safety::setSafeDistance(int d){
+    m_currentDistance = d;
 }
--- src/services/safety/safety.h
+++ src/services/safety/safety.h
@@ -7,10 +7,12 @@ public:
     void onAttach()override;
     void onUpdate()override;
 
+    void setSafeDistance(int safeDistance);
+
     Safety(): Service(ID_SAFETY){};
     SERVICE(Safety)
 
 private:
-
+    int m_currentDistance;
 };
 EXTERN_DECLARATION(Safety, safety)
